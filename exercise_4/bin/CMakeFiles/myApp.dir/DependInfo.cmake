732a9ee0cb86c2c5ec6fc311578308de1d2cee59
kendrick807 <tkogt12@gmail.com>
2022-11-24 10:37:51 +0200

    added exercise 4

---
+++ exercise_4/bin/CMakeFiles/myApp.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kendrick807/lab2/embedded-linux/exercise_4/main.c" "CMakeFiles/myApp.dir/main.c.o" "gcc" "CMakeFiles/myApp.dir/main.c.o.d"
+  "/home/kendrick807/lab2/embedded-linux/exercise_4/randomNum.c" "CMakeFiles/myApp.dir/randomNum.c.o" "gcc" "CMakeFiles/myApp.dir/randomNum.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ exercise_4/bin/CMakeFiles/myApp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myApp.dir/main.c.o"
+  "CMakeFiles/myApp.dir/main.c.o.d"
+  "CMakeFiles/myApp.dir/randomNum.c.o"
+  "CMakeFiles/myApp.dir/randomNum.c.o.d"
+  "myApp"
+  "myApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
